07b21b3e6038216828817855aa9a0bc4d5265aee
EffEmPee <felipemacacari@gmail.com>
2020-09-15 13:28:34 -0300

    reactjs-rocketSeat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './styles.css';
+
+import Header from './components/Header';
+import Main from './pages/main';
+
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+);
+
 
 export default App;
